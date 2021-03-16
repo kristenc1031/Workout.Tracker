d3cf181c2a33c3a9f37c25f007a75b9429b345af
Kristen Campbell <kristenc1031@gmail.com>
2021-03-16 15:46:14 -0600

    range route

--- routes/api.js
+++ routes/api.js
@@ -24,6 +24,16 @@ router.get("/api/workouts", (req, res) => {
     ]).then(workout => res.json(workout)).catch(err => res.json (err))
 })
 
-
+router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration",
+                }
+            }
+        }
+    ]).sort({_id: -1}).limit(7).then(workout => res.json(workout)).catch(err => res.json (err))
+})
 
 module.exports = router;
