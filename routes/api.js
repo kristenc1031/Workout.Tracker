dc6e6655ca0cfe1bfacfda8d002f1300ddf147f0
Kristen Campbell <kristenc1031@gmail.com>
2021-03-16 15:39:28 -0600

    api route

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const Workout = require("../models/workout");
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create({}).then(workout => res.json(workout)
+    ).catch(err => res.json (err))
+})
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(
+        req.params.id,
+        { $push: { exercises: req.body } },
+        {new: true, runValidators: true}
+    ).then(workout => res.json(workout)).catch(err => res.json (err))
+}) 
